41dc3c274b11820635d994c657463f138fe4e1f7
jinjiuling <65697148+jinjiuling@users.noreply.github.com>
2021-04-23 10:05:41 +0800

    Add files via upload

---
+++ config/prefs.php
@@ -0,0 +1,9 @@
+<?php
+$GLOBALS['lang'] = 'cn';
+$GLOBALS['auteur'] = 'admin';
+$GLOBALS['email'] = 'mail@example.com';
+$GLOBALS['racine'] = 'http://3lqeyh3445.qq.com/1/';
+$GLOBALS['format_date'] = '0';
+$GLOBALS['format_heure'] = '0';
+$GLOBALS['fuseau_horaire'] = 'UTC';
+?>
